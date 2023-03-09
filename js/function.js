98f4b9dac5ad3dd9eefd13c99feca996bb85c05e
elijt1994 <joshpleing@gmail.com>
2023-03-09 00:13:17 -0300

    Trabajo Final
    v 2.0

--- js/function.js
+++ js/function.js
@@ -1,4 +1,30 @@
+document.getElementById('button_1').onclick = function() {
+    document.getElementById('txtbt_1').innerHTML = "Desarrollador independiente de html en sitios freelancer | 2008-Actualidad";
+}
 
+document.getElementById('button_2').onclick = function() {
+    document.getElementById('txtbt_2').innerHTML = "Encargado de mantenimiento en web e-commerce dedicada a venta de insumos gastronómicos | 2010-2015";
+}
+
+document.getElementById('button_3').onclick = function() {
+    document.getElementById('txtbt_3').innerHTML = "Participación en desarrollo de JS para modificación de interface e integración con sistemas ERP, CRM, OCR y DMS | 2018-2019";
+}
+
+document.getElementById('button_4').onclick = function() {
+    document.getElementById('txtbt_4').innerHTML = "Creador y editor de imágenes, banners y logos plublicitarios para tienda online | 2011-Actualidad";
+}
+
+document.getElementById('edubt_1').onclick = function() {
+    document.getElementById('txtedubt_1').innerHTML = "Universidad Tecnológica Nacional | 2005-2008";
+}
+
+document.getElementById('edubt_2').onclick = function() {
+    document.getElementById('txtedubt_2').innerHTML = "Instituto Superior CADS | 2008-2011";
+}
+
+
+
+/** 
 let button_1 = document.getElementById('button_1');
 
 let tbt_1 = document.getElementById('tbt_1');
